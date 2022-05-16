146f39865b1d82bf0a48ca86d5c47939e63ce420
DoryBoy <williamodaw@outlook.com>
2022-05-16 15:18:46 +0100

    Update Main.java

--- src/com/williamdaw/Main.java
+++ src/com/williamdaw/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String value = scanner.next();
+        String value = new Scanner(System.in).next();
         StringBuilder sb = new StringBuilder(value);
         if ((value.equalsIgnoreCase(String.valueOf(sb.reverse())))) System.out.println("is a palindrome");
     }
